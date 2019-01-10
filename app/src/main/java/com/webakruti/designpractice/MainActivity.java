22fe9ba82c43e1d496e2c1e928e3ca3a71dcaaae
DELL <palluwaghaye1104@gmail.com>
2019-01-10 19:04:14 +0530

    'menu_dots'

--- app/src/main/java/com/webakruti/designpractice/MainActivity.java
+++ app/src/main/java/com/webakruti/designpractice/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,11 +30,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonGoToPopupPage;
     private Button buttonGotoFloatingButton;
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         buttonGoToPopupPage = (Button)findViewById(R.id.buttonGoToPopupPage);
         buttonGoToPopupPage.setOnClickListener(new View.OnClickListener() {
@@ -125,5 +136,36 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,list2);
         spinner2.setAdapter(adapter2);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dotmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.home:
+                Intent intent = new Intent(MainActivity.this,FloatingActionButtonActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.settings:
+                Intent intent1 = new Intent(MainActivity.this,EditProfileActivity.class);
+                startActivity(intent1);
+                finish();
+                break;
+            case R.id.help:
+                Intent intent2 = new Intent(MainActivity.this,EditProfileActivity.class);
+                startActivity(intent2);
+                finish();
+                break;
+        }
+
+        return true;
     }
 }
